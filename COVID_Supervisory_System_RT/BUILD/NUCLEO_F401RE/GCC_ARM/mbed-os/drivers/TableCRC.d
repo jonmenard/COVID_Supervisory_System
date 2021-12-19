f6bfb1adeb27e7494e4f862dc20ea5a8b568d095
Jon Menard <jonmenard@cmail.carleton.ca>
2021-12-19 17:01:37 -0500

    Delete COVID_Supervisory_System_RT directory

--- COVID_Supervisory_System_RT/BUILD/NUCLEO_F401RE/GCC_ARM/mbed-os/drivers/TableCRC.d
+++
@@ -1,4 +0,0 @@
-..\BUILD\NUCLEO_F401RE\GCC_ARM\mbed-os\drivers\TableCRC.o: \
- ..\mbed-os\drivers\TableCRC.cpp \
- ..\BUILD\NUCLEO_F401RE\GCC_ARM\mbed_config.h \
- ../mbed-os/drivers/TableCRC.h
--- COVID_Supervisory_System_RT/BUILD/NUCLEO_F401RE/GCC_ARM/mbed-os/platform/mbed_wait_api_rtos.d
+++
@@ -1,3 +0,0 @@
-..\BUILD\NUCLEO_F401RE\GCC_ARM\mbed-os\platform\mbed_wait_api_rtos.o: \
- ..\mbed-os\platform\mbed_wait_api_rtos.cpp \
- ..\BUILD\NUCLEO_F401RE\GCC_ARM\mbed_config.h
